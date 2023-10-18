b22f6c0cb308073446ddbdee7896f60c6043259d
dreaMykj <gabrielgreghi2018@gmail.com>
2023-10-18 11:48:33 -0300

    Concluido

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int v,d,t;
+
+Console.Write("Distância percorrida (m): ");
+d = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Tempo gasto (s): ");
+t = Convert.ToInt32(Console.ReadLine());
+
+v = d / t;
+Console.Write($"Velocidade média: {v} m/s ");
